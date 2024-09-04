a7541e52a7816ad6e12c93d78b73cc353647cce3
KanaiMint <k022g0015@g.neec.ac.jp>
2024-09-04 14:19:19 +0900

    test

--- Assets/mintest.cs
+++ Assets/mintest.cs
@@ -7,7 +7,7 @@ public class mintest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       // aaaaaaaaaa;
     }
 
     // Update is called once per frame
