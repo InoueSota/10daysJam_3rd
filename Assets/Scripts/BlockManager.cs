15afab51613a0f03e5ff3455aea997c08c190c16
KanaiMint <115054962+KanaiMint@users.noreply.github.com>
2024-09-06 06:22:52 +0900

    ブロックが消える演出追加。しかしブロックのisActive の処理がかみあわなかっだし

--- Assets/Scripts/BlockManager.cs
+++ Assets/Scripts/BlockManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,7 @@ public class BlockManager : MonoBehaviour
     // 自コンポーネント取得
     private AllObjectManager allObjectManager;
     private SpriteRenderer spriteRenderer;
-
+    private S_BlockDestory blockDestory;
     void Start()
     {
         allObjectManager = GetComponent<AllObjectManager>();
@@ -33,8 +34,22 @@ public class BlockManager : MonoBehaviour
         {
             //
             // ここが消された瞬間
+            //blockDestory.BlockDestroy(spriteRenderer.enabled);
+            //Sequenceのインスタンスを作成
+            var sequence = DOTween.Sequence();
+
+            //Appendで動作を追加していく
+            sequence.Append(transform.DOScale(Vector3.zero, 0.35f).SetEase(Ease.OutBack));
+            //Joinはひとつ前の動作と同時に実行される
+            sequence.Join(this.transform.DORotate(Vector3.forward * 360, 0.35f, RotateMode.LocalAxisAdd).SetEase(Ease.InSine));
+            sequence.Join(this.GetComponent<SpriteRenderer>().DOFade(endValue: 0, duration: 0.35f).SetEase(Ease.InQuad));
+
+            sequence.Play().OnComplete(() =>
+            {
+                spriteRenderer.enabled = false;
+            });
             //
-            spriteRenderer.enabled = false;
+
         }
         allObjectManager.SetIsActive(spriteRenderer.enabled);
     }
