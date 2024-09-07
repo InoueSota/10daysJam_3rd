2d44bc359a61f3090a653ab159a982bb8aba02c2
ShigaMitsuki <k022g0049@g.neec.ac.jp>
2024-09-08 01:02:30 +0900

    草パーティクルまで

--- Assets/3_Scripts/Shiga/GrassParentManager.cs
+++ Assets/3_Scripts/Shiga/GrassParentManager.cs
@@ -16,18 +16,27 @@ public class GrassParentManager : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         particle = GetComponent<ParticleSystem>();
 
-        Disappear();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+
+            Disappear();
+        }
+
     }
 
     void Disappear()
     {
+
         particle.Play();
         spriteRenderer.enabled = false;
+
+
     }
 
 }
