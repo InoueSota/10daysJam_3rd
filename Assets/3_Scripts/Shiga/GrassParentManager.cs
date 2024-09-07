60fb9421d761f78116ffdd873a815b046cd5d4c3
ShigaMitsuki <k022g0049@g.neec.ac.jp>
2024-09-08 00:44:29 +0900

    草

--- Assets/3_Scripts/Shiga/GrassParentManager.cs
+++ Assets/3_Scripts/Shiga/GrassParentManager.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class GrassParentManager : MonoBehaviour
 {
 
+    private SpriteRenderer spriteRenderer;
+
+    ParticleSystem particle = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        particle = GetComponent<ParticleSystem>();
+
+        Disappear();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+
+    void Disappear()
+    {
+        particle.Play();
+        spriteRenderer.enabled = false;
+    }
+
 }
