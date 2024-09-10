f8a2766a5f35c7da1ab0201794a3ec02589134a8
InoueSota <k022g0045@g.neec.ac.jp>
2024-09-10 23:30:14 +0900

    全ステージシーンにMenuをアタッチ！アタッチ！アタッチィィ！

--- Assets/3_Scripts/AllObjectManager.cs
+++ Assets/3_Scripts/AllObjectManager.cs
@@ -14,6 +14,7 @@ public class AllObjectManager : MonoBehaviour
         GRASSPARENT,
         DRIPSTONE,
         BOMB,
+        SPLIT,
         ICICLE
     }
     [SerializeField] private ObjectType objectType;
