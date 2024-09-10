7598b935fd9fae51bfe8a01967688279f2ff49d8
InoueSota <k022g0045@g.neec.ac.jp>
2024-09-11 02:17:25 +0900

    避けブロックがいかにも避けそうになった

--- Assets/3_Scripts/SplitManager.cs
+++ Assets/3_Scripts/SplitManager.cs
@@ -5,26 +5,73 @@ public class SplitManager : MonoBehaviour
     // 自コンポーネント取得
     private AllObjectManager allObjectManager;
 
-    // 子オブジェクト取得
+    [Header("子オブジェクト取得")]
     [SerializeField] private GameObject overObj;
     private SpriteRenderer overSpriteRenderer;
+    private Transform overTransform;
+    private Vector3 overOrigin;
+    private Vector3 overTarget;
     [SerializeField] private GameObject underObj;
     private SpriteRenderer underSpriteRenderer;
+
+    // 他オブジェクト取得
+    private Transform playerTransform;
 
     // フラグ - 上にオブジェクトがあるか
     private bool isFreeOver;
 
+    [Header("予告避け")]
+    [SerializeField] private float floatHeight;
+    [SerializeField] private float chasePower;
+    [SerializeField] private float flowSpeed;
+    [SerializeField] private float range;
+    private float angle;
+
     void Start()
     {
         allObjectManager = GetComponent<AllObjectManager>();
+
+        // 子オブジェクト初期化
         overSpriteRenderer = overObj.GetComponent<SpriteRenderer>();
+        overTransform = overObj.transform;
+        overOrigin = overObj.transform.position;
+        overTarget = overOrigin;
         underSpriteRenderer = underObj.GetComponent<SpriteRenderer>();
+
+        // プレイヤーの位置を取得
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+        // 初期化時は上にオブジェクトがない判定にする
         isFreeOver = true;
+
+        // sin運動フワフワ初期化
+        angle = 0;
     }
 
     void Update()
     {
-        Debug.Log(isFreeOver);
+        NoticeFreeOver();
+    }
+    void NoticeFreeOver()
+    {
+        float yBetween = Mathf.Abs(transform.position.y - playerTransform.position.y + 1f);
+
+        if (isFreeOver && yBetween < 0.4f)
+        {
+            overTarget.y = overOrigin.y + floatHeight;
+
+            // Targetをsin運動させる
+            angle += flowSpeed * Time.deltaTime;
+            overTarget.y += Mathf.Sin(angle) * range;
+        }
+        else
+        {
+            overTarget = overOrigin;
+            angle = 0f;
+        }
+
+        // Targetに向かって追い続ける
+        overTransform.position += (overTarget - overTransform.position) * (chasePower * Time.deltaTime);
     }
 
     // 初期化処理
@@ -34,7 +81,9 @@ public class SplitManager : MonoBehaviour
         underSpriteRenderer.enabled = true;
         allObjectManager.SetIsActive(overSpriteRenderer.enabled);
         allObjectManager.Initialize();
+
         isFreeOver = true;
+        angle = 0;
     }
 
     // 消滅処理
