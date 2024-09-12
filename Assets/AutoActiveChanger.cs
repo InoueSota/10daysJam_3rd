f944fb13cd801b05129b1623870b2a8006fc4f90
ShigaMitsuki <k022g0049@g.neec.ac.jp>
2024-09-12 17:21:09 +0900

    つららとか

---
+++ Assets/AutoActiveChanger.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoActiveChanger : MonoBehaviour
+{
+
+    [SerializeField] AllObjectManager allObjectManager;
+    ParticleSystem particle;
+    SpriteRenderer spriteRenderer;
+
+    bool preActive;
+    bool isActive;
+
+    [SerializeField] bool particleClear = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        preActive = isActive;
+        isActive = allObjectManager.GetIsActive();
+
+        if (preActive != isActive)
+        {
+            if (isActive == true)
+            {
+                if (particle != null)
+                {
+                    particle.Play();
+                }
+                if (spriteRenderer != null)
+                {
+                    spriteRenderer.enabled = true;
+                }
+            }
+            else
+            {
+                if (particle != null)
+                {
+                    particle.Stop();
+                    if(particleClear == true)
+                    {
+                        particle.Clear();
+                    }
+                }
+                if (spriteRenderer != null)
+                {
+                    spriteRenderer.enabled = false;
+                }
+            }
+        }
+    }
+}
